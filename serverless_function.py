93184856a0eb15b05b776782ec85e937a66dc313
Henrique Fagundes <lukamone021@hotmail.com>
2022-07-17 22:14:43 +0000

    Add streamlit file

--- serverless_function.py
+++ serverless_function.py
@@ -7,9 +7,9 @@ from datetime import datetime, timedelta
 RELEVANT_KEYS = ['cpu_percent-0', 'cpu_percent-1', 'cpu_percent-2', 'cpu_percent-3', 'cpu_percent-4', 'cpu_percent-5', 'cpu_percent-6', 'cpu_percent-7', 'cpu_percent-8', 'cpu_percent-9', 'cpu_percent-10', 'cpu_percent-11', 'cpu_percent-12', 'cpu_percent-13', 'cpu_percent-14', 'cpu_percent-15', 'cpu_freq_current', 'n_pids']
 
 def handler(input, context):
-    env = context["env"]
+    env = context.env
     # env = {'cpu_percent-0': {'2022-07-16 21:54:54.869218': {'60sec': 85.7, '60min': 33.9}}}
-    metrics = json.loads(input)
+    metrics = input
     '''
     {'timestamp': '2022-07-16 19:08:54.869218', 'cpu_percent-0': 1.0, 'cpu_percent-1': 2.2, 'cpu_percent-2': 1.0, 'cpu_percent-3': 4.0, 'cpu_percent-4': 1.4, 'cpu_percent-5': 1.4, 'cpu_percent-6': 1.8, 'cpu_percent-7': 2.6, 'cpu_percent-8': 0.6, 'cpu_percent-9': 2.6, 'cpu_percent-10': 1.8, 'cpu_percent-11': 1.8, 'cpu_percent-12': 0.8, 'cpu_percent-13': 2.6, 'cpu_percent-14': 4.0, 'cpu_percent-15': 1.0, 'cpu_freq_current': 2666.760000000001, 'cpu_stats-ctx_switches': 15545135406, 'cpu_stats-interrupts': 8515418247, 'cpu_stats-soft_interrupts': 2801393181, 'cpu_stats-syscalls': 0, 'virtual_memory-total': 20986593280, 'virtual_memory-available': 13110468608, 'virtual_memory-percent': 37.5, 'virtual_memory-used': 7479259136, 'virtual_memory-free': 323936256, 'virtual_memory-active': 9059991552, 'virtual_memory-inactive': 7857815552, 'virtual_memory-buffers': 1236148224, 'virtual_memory-cached': 11947249664, 'virtual_memory-shared': 11337728, 'virtual_memory-slab': 3410583552, 'n_pids': 863, 'net_io_counters_eth0-bytes_sent': 3126008909, 'net_io_counters_eth0-bytes_recv': 10043666039, 'net_io_counters_eth0-packets_sent': 4524828, 'net_io_counters_eth0-packets_recv': 6864541, 'net_io_counters_eth0-errin': 0, 'net_io_counters_eth0-errout': 0, 'net_io_counters_eth0-dropin': 58, 'net_io_counters_eth0-dropout': 0}
     '''
---
+++ streamlit.py
@@ -0,0 +1,65 @@
+import altair as alt
+import streamlit as st
+import pandas as pd
+import redis
+import json
+from time import sleep
+
+st.title('Monitoring Dashboard')
+holder = st.empty()
+holder2 = st.empty()
+holder3 = st.empty()
+
+past_metrics_60sec = {'cpu0': [], 'cpu1': [], 'cpu2': [], 'cpu3': [], 'cpu4': [], 'cpu5': [], 'cpu6': [], 'cpu7': [], 'cpu8': [], 'cpu9': [], 'cpu10': [], 'cpu11': [], 'cpu12': [], 'cpu13': [], 'cpu14': [], 'cpu15': [], 'n_pids': []}
+past_metrics_60min = {'cpu0': [], 'cpu1': [], 'cpu2': [], 'cpu3': [], 'cpu4': [], 'cpu5': [], 'cpu6': [], 'cpu7': [], 'cpu8': [], 'cpu9': [], 'cpu10': [], 'cpu11': [], 'cpu12': [], 'cpu13': [], 'cpu14': [], 'cpu15': [], 'n_pids': []}
+
+while True:
+    # Fetch Redis data
+    redis_data = redis.Redis(host='192.168.121.189', port='6379').get('augustolessa-proj3-output')
+    json_data = json.loads(redis_data)
+
+    json_data.pop('avg-util-cpu_freq_current-60sec')
+    json_data.pop('avg-util-cpu_freq_current-60min')
+
+    # Setup cpu df data
+    data = {}
+    filtered_data_60sec = {k: v for k, v in json_data.items() if k.startswith('avg-util-cpu') and k.endswith('60sec')}
+    filtered_data_60sec = {k.split("-")[2]:v for k,v in filtered_data_60sec.items()}
+    for k,v in filtered_data_60sec.items():
+        data[k] = [*past_metrics_60sec[k]]
+        data[k].append(v)
+        past_metrics_60sec[k].append(v)
+        if len(past_metrics_60sec[k]) > 10:
+            past_metrics_60sec[k].pop(0)
+
+    df = pd.DataFrame.from_dict(data)
+    holder.line_chart(df)
+    
+    # Setup cpu df data
+    data = {}
+    filtered_data_60min = {k: v for k, v in json_data.items() if k.startswith('avg-util-cpu') and k.endswith('60min')}
+    filtered_data_60min = {k.split("-")[2]:v for k,v in filtered_data_60min.items()}
+    for k,v in filtered_data_60min.items():
+        data[k] = [*past_metrics_60min[k]]
+        data[k].append(v)
+        past_metrics_60min[k].append(v)
+        if len(past_metrics_60min[k]) > 10:
+            past_metrics_60min[k].pop(0)
+
+    df = pd.DataFrame.from_dict(data)
+    holder2.line_chart(df)
+
+    # Setup pid df data
+    data = {}
+    filtered_data_pids = {k: v for k, v in json_data.items() if k.startswith('avg-util-n_pids')}
+    filtered_data_pids = {k.split("-")[2]:v for k,v in filtered_data_pids.items()}
+    data["n_pids_60sec"] = [*past_metrics_60sec["n_pids"]]
+    data["n_pids_60sec"].append(filtered_data_pids["n_pids"])
+    past_metrics_60sec["n_pids"].append(filtered_data_pids["n_pids"])
+    if len(past_metrics_60sec["n_pids"]) > 10:
+        past_metrics_60sec["n_pids"].pop(0)
+
+    df = pd.DataFrame.from_dict(data)
+    holder3.line_chart(df)
+
+    sleep(10)
